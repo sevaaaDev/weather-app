be6e095e9a4bfda86ed505b6c19ee2a069cc3333
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-10-10 18:20:05 +0700

    Add: fetch weather function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 const key = "6104b68df4be4b02b7a52853231010";
+async function getTodayWeather(place) {
+  let dataJson = await fetchWeather(`current.json?key=${key}&q=${place}`);
+  console.log(dataJson);
+  displayTodayWeather(dataJson);
+}
+async function fetchWeather(query) {
+  try {
+    let response = await fetch(`http://api.weatherapi.com/v1/${query}`);
+    if (!response.ok) {
+      throw Error(response.status);
+    }
+    let dataJson = await response.json();
+    return dataJson;
+  } catch (err) {
+    console.log(err);
+  }
+}
