964be9750336a785971673915fb695ac52b22115
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-10-18 11:50:45 +0700

    Add: form listener

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import { displayTodayWeather } from "./displayController";
 
 const input = document.querySelector("input");
 const btn = document.querySelector("button");
+const form = document.querySelector(".search");
 btn.addEventListener("click", () => {
-  if (!input.value) return;
-  displayTodayWeather(input.value);
+  fetchData(input);
 });
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetchData(input);
+});
+
+function fetchData(input) {
+  if (!input.value) return;
+  displayTodayWeather(input.value);
+}
 displayTodayWeather("bandung");
 // displayDaysForecastWeather("bandung");
 // displayHoursForecastWeather("bandung");
