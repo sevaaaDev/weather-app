804d9c744785f2f1decbdefb0a915d7e272b5cfc
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-10-18 11:26:19 +0700

    Fix: the api

--- src/getWeatherData.js
+++ src/getWeatherData.js
@@ -2,9 +2,7 @@ import { getDay, format, getHours } from "date-fns/esm";
 const key = "6104b68df4be4b02b7a52853231010";
 async function fetchWeather(query) {
   try {
-    const response = await fetch(`http://api.weatherapi.com/v1/${query}`, {
-      cache: "no-cache",
-    });
+    const response = await fetch(`http://api.weatherapi.com/v1/${query}`);
     if (!response.ok) {
       throw new Error(response.statusText);
     }
@@ -34,11 +32,18 @@ function filterJson(json) {
   ];
   let forecast = json.forecast;
   let obj = {
-    Location: json.location.name,
+    FeelsC: `${Math.floor(json.feelslike_c)}°C`,
+    FeelsF: `${Math.floor(json.feelslike_f)}°F`,
+    Location: `${json.location.name}, ${json.location.country}`,
     Condition: json.current.condition.text,
-    "Local Time": format(new Date(json.location.localtime), "H:m"),
-    TemperatureC: `${json.current.temp_c}°C`,
-    TemperatureF: `${json.current.temp_f}°F`,
+    "Local Time": format(
+      new Date(json.location.localtime),
+      "EEEE, d MMM yy | HH:mm",
+    ),
+    TemperatureC: `${Math.floor(json.current.temp_c)}°C`,
+    TemperatureF: `${Math.floor(json.current.temp_f)}°F`,
+    WindKPH: `${json.current.wind_kph} KpH`,
+    WindMPH: `${json.current.wind_mph} MpH`,
     Humidity: `${json.current.humidity}%`,
     ChanceOfRain: `${forecast.forecastday[0].day.daily_chance_of_rain}%`,
     forecastDay: [
